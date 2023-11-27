d93e515fec5bd7fbd5205d68fc3de981de0effbe
newellna <110679599+newellna@users.noreply.github.com>
2023-11-27 10:58:22 -0500

    Add files via upload

---
+++ WhileLoop.java
@@ -0,0 +1,57 @@
+package whileloop;
+import java.util.Scanner;
+
+public class WhileLoop {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner (System.in); 
+		while (true)
+		{
+			System.out.println("1. Science Fiction");
+			System.out.println("2. Computers and Technology");
+			System.out.println("3. Cooking");
+			System.out.println("4. Business");
+			System.out.println("5. Comics");
+			System.out.println("6. Exit");
+			
+			System.out.println("Which category do you choose?");
+			String input = sc.nextLine();
+			int category = Integer.parseInt(input);
+			
+			if(category==1)
+			{
+				System.out.println("Science fiction is a futuristic choice!");
+			}
+			else if (category==2)
+			{
+				System.out.println("Computer and Technology is a techy choice!");
+			}
+			else if (category==3)
+			{
+				System.out.println("Cooking is a delicious choice!");
+			}
+			else if (category==4)
+			{
+				System.out.println("Business is a smart choice!");
+			}
+			else if (category==5)
+			{
+				System.out.println("Comics is a nerdy choice!"); // Comic readers are not nerds! 
+			}
+			else if (category==6)
+			{
+				System.out.println("Exiting the program");
+				break;
+			}
+			else
+			{
+				System.out.println("You selected an invalid number");
+			}
+			
+			System.out.println();
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
